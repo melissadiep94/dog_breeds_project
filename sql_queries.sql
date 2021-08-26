bf6a086165d75cc2efaf5511a1812c35f159fdd8
applepassion4 <53684246+applepassion4@users.noreply.github.com>
2021-08-26 00:11:37 -0400

    Adding sql queries

---
+++ sql_queries.sql
@@ -0,0 +1,20 @@
+
+
+SELECT breeds.breed_id, breeds.breed_name, lifespan.avg_lifespan_years
+FROM lifespan
+INNER JOIN breeds ON
+breeds.breed_id = lifespan.breed_id
+
+SELECT lifespan.breed_id, lifespan.breed_name, lifespan.avg_lifespan_years
+FROM lifespan
+WHERE avg_lifespan_years = '10 to 13 yrs'
+
+SELECT weight.breed_id, weight.breed_name, weight.weight_male
+FROM weight
+WHERE weight_male = '40-65 pounds'
+
+SELECT pet_finder.breed_name, lifespan.avg_lifespan_years
+FROM lifespan
+INNER JOIN pet_finder ON
+pet_finder.breed_name = lifespan.breed_name
+WHERE avg_lifespan_years = '12 to 15 yrs'
